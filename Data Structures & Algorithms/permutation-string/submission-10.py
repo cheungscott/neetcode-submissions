ba6d2a6931f97eacf8ed635c1bfbae98056c6168
Scott Cheung Chun Wah <134698692+cheungscott@users.noreply.github.com>
2026-08-18 01:25:38 +0100

    Add: permutation-string - submission-10

---
+++ Data Structures & Algorithms/permutation-string/submission-10.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        counts1, counts2 = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            counts1[ord(s1[i]) - ord("a")] += 1
+            counts2[ord(s2[i]) - ord("a")] += 1
+        matches = 0
+        for i in range(26):
+            if counts1[i] == counts2[i]:
+                matches += 1
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            counts2[ord(s2[r]) - ord("a")] += 1
+            if counts2[ord(s2[r]) - ord("a")] == counts1[ord(s2[r]) - ord("a")]:
+                matches += 1
+            elif counts2[ord(s2[r]) - ord("a")] == counts1[ord(s2[r]) - ord("a")] + 1:
+                matches -= 1
+
+            counts2[ord(s2[l]) - ord("a")] -= 1
+            if counts1[ord(s2[l]) - ord("a")] == counts2[ord(s2[l]) - ord("a")]:
+                matches += 1
+            elif counts1[ord(s2[l]) - ord("a")] - 1 == counts2[ord(s2[l]) - ord("a")]:
+                matches -= 1
+            l += 1
+        return matches == 26
