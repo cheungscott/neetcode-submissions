052b3c0c371f574147de282ebc574a21c25193be
Scott Cheung Chun Wah <134698692+cheungscott@users.noreply.github.com>
2026-09-04 20:40:27 +0100

    Add: candy-crush - submission-1

---
+++ Data Structures & Algorithms/candy-crush/submission-1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(board), len(board[0])
+
+        def find_and_crush():
+            complete = True
+
+            for r in range(1, ROWS - 1):
+                for c in range(COLS):
+                    if board[r][c] == 0:
+                        continue
+                    if abs(board[r][c]) == abs(board[r-1][c]) == abs(board[r+1][c]):
+                        (board[r][c]) = -abs(board[r][c])
+                        (board[r-1][c]) = -abs(board[r-1][c])
+                        (board[r+1][c]) = -abs(board[r+1][c])
+                        complete = False
+            for r in range(ROWS):
+                for c in range(1, COLS - 1):
+                    if board[r][c] == 0:
+                        continue
+                    if abs(board[r][c]) == abs(board[r][c-1]) == abs(board[r][c+1]):
+                        (board[r][c]) = -abs(board[r][c])
+                        (board[r][c-1]) = -abs(board[r][c-1])
+                        (board[r][c+1]) = -abs(board[r][c+1])
+                        complete = False
+            for r in range(ROWS):
+                for c in range(COLS):
+                    if board[r][c] < 0:
+                        board[r][c] = 0
+            return complete
+        
+        def drop():
+            for c in range(COLS):
+                lowest_zero = -1
+                for r in range(ROWS-1, -1, -1):
+                    if board[r][c] == 0:
+                        lowest_zero = max(lowest_zero, r)
+                    elif lowest_zero >= 0:
+                        board[r][c], board[lowest_zero][c] = board[lowest_zero][c], board[r][c]
+                        lowest_zero -= 1
+        while not find_and_crush():
+            drop()
+        return board
